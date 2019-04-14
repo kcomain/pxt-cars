0ab23f1b289a244e2fbdd6d2613ce5f883921127
Kenny Cheung <cheunghowui@gmail.com>
2019-04-14 21:10:47 +0800

    Automatic commit flow at 20190414_091046

--- commit.ps1
+++ commit.ps1
@@ -113,7 +113,6 @@ Write-Progress -Activity "Pushing..." -Id $progID -PercentComplete $rand  -error
 $randtime=Get-Random -Minimum 100 -Maximum 500
 Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
-cls
 Write-Host -BackgroundColor black -ForegroundColor green -Object "....................................."
 Write-Host -BackgroundColor black -ForegroundColor green -Object ".████.....██████...████..██...██████."
 Write-Host -BackgroundColor black -ForegroundColor green -Object ".██..██...██..██...██.██.██...██....."
