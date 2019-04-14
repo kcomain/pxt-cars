d780b9f882e932be34dcb44ba280758c8aca0a50
Kenny Cheung <cheunghowui@gmail.com>
2019-04-14 20:34:14 +0800

    Automatic commit flow at 20190414_083414

--- commit.ps1
+++ commit.ps1
@@ -25,31 +25,63 @@ Write-Progress -Activity "Get Directory, $LOCA" -Id $progID -PercentComplete $ra
 $rand=Get-Random -Minimum ($randinit+1) -Maximum ($randinit+10)
 Write-Progress -Activity "Staging files..." -Id $progID -PercentComplete 40 -SecondsRemaining 100
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 Write-Progress -Activity "Staging files..." -Id $progID -PercentComplete 50 -SecondsRemaining 100
 git add *
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 Write-Progress -Activity "Committing..." -Id $progID -PercentComplete $rand -SecondsRemaining 100
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
 Write-Progress -Activity "Committing..." -Id $progID -PercentComplete $rand -SecondsRemaining 100
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
 Write-Progress -Activity "Committing..." -Id $progID -PercentComplete $rand -SecondsRemaining 100
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 $time=Get-Date -Format 'yyyyMMdd_hhmmss'
 git commit -m "Automatic commit flow at $time"
 Write-Progress -Activity "Committing..." -Id $progID -PercentComplete $rand -SecondsRemaining 100
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
 Write-Progress -Activity "Committing..." -Id $progID -PercentComplete $rand -SecondsRemaining 100
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
 Write-Progress -Activity "Committing..." -Id $progID -PercentComplete $rand -SecondsRemaining 100
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
 Write-Progress -Activity "Committing..." -Id $progID -PercentComplete $rand -SecondsRemaining 100
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
 Write-Progress -Activity "Pushing..." -Id $progID -PercentComplete $rand -SecondsRemaining 100
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
 Write-Progress -Activity "Pushing..." -Id $progID -PercentComplete $rand -SecondsRemaining 100
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
 Write-Progress -Activity "Pushing..." -Id $progID -PercentComplete $rand -SecondsRemaining 100
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
 Write-Progress -Activity "Pushing..." -Id $progID -PercentComplete $rand -SecondsRemaining 100
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
 Write-Progress -Activity "Pushing..." -Id $progID -PercentComplete $rand -SecondsRemaining 100
+$randtime=Get-Random -Minimum 100 -Maximum 1000
+Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
