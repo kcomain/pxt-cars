6e54d615a0d9961caf84f38032b7f7465673fba9
Kenny Cheung <cheunghowui@gmail.com>
2019-04-14 19:52:07 +0800

    Automatic commit flow at 19:52:07

---
+++ commit.ps1
@@ -0,0 +1,30 @@
+# Defining Variables...
+
+$LOCA = $PWD
+
+function timestamp() {
+
+  date +"%T"
+
+}
+
+# Real Code HERE
+
+echo "Locating..."
+
+echo "I think the current location is $LOCA."
+
+echo "Staging Files..."
+
+git add *
+
+echo "Committing Changes..."
+
+git commit -m "Automatic commit flow at $(timestamp)"
+echo "Pushing commits......"
+
+git push
+
+echo "DONE"
+
+return 0
