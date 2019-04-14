801092ecac68d3c5e1a1aadeac7f3c5825590b68
Kenny Cheung <cheunghowui@gmail.com>
2019-04-14 20:37:01 +0800

    Automatic commit flow at 20190414_083700

--- commit.ps1
+++ commit.ps1
@@ -106,5 +106,6 @@ Write-Progress -Activity "Pushing..." -Id $progID -PercentComplete $rand
 $randtime=Get-Random -Minimum 100 -Maximum 500
 Start-Sleep -Milliseconds $randtime
 $rand=Get-Random -Minimum ($rand+1) -Maximum ($rand+10)
+cls
 echo "All Done."
 return 0
