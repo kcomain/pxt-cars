4669749bfcec5e3e977233b54c2374f848372fe9
Kenny <cheunghowui@gmail.com>
2019-05-05 11:24:58 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -4,28 +4,29 @@
 // //[%] v.min=0 v.max= 42 x.defl=25
 namespace cars {
     /**
-     * TODO: describe your function here
-     * @param StopAll "Stop Cars"
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
+     * Stop the car
      */
-
     //% block
-    
+
     export function StopAll(): void {
         pins.analogWritePin(AnalogPin.P0, 0)
         pins.analogWritePin(AnalogPin.P1, 0)
         pins.analogWritePin(AnalogPin.P2, 0)
         pins.analogWritePin(AnalogPin.P8, 0)
     }
-
+    /**
+     * Turn Right with only left wheel
+    */
     //% block
     export function LeftWheelForwardWithSpeed(speed: number): void {
         pins.analogWritePin(AnalogPin.P8, speed)
         pins.digitalWritePin(DigitalPin.P2, 0)
     }
+    /**
+     * Turn Left with only left wheel
+     */
     //% block
-    export function LeftWheelBackwardWithSpeed(speed: number): void {
+    export function Left_Wheel_Backward_With_Speed(speed: number): void {
         pins.analogWritePin(AnalogPin.P2, speed)
         pins.digitalWritePin(DigitalPin.P8, 0)
     }
